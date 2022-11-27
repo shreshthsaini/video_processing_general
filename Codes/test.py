e840c5bbc379117865e295ae2c252ebdb5167c78
shreshth saini <shreshthfeel@gmial.com>
2022-11-27 15:02:36 -0600

    code sample complete

--- Codes/test.py
+++ Codes/test.py
@@ -1,2 +1,19 @@
+import os 
+import numpy as np 
+import cv2 
 
+
+img = cv2.imread("/Users/shreshthsaini/Desktop/DeltaE_8bit_gamma2.2.tif")
+print(img.shape)
+
+def filtering(img,kernel):
+    operated = cv2.filter2D(img, -1, kernel)
+    return operated
+
+
+#mean filtering 
+kernel = np.ones((20,20),np.float32)/(20*20)
+
+#cv2.imshow('mean_blur', filtering(img, kernel))
+cv2.imwrite('mean_blur.png', filtering(img, kernel))
 
